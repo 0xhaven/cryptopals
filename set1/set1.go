20490db47ebeca6d37ea008d365579e204f81768
Jacob H. Haven <jacob@cloudflare.com>
2014-10-20 18:59:55 -0700

    Set 1, XorArrays

--- set1/set1.go
+++ set1/set1.go
@@ -3,6 +3,7 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 )
 
 func HexToBase64(hexStr string) (string, error) {
@@ -12,4 +13,21 @@ func HexToBase64(hexStr string) (string, error) {
 	}
 	var base64Str = base64.StdEncoding.EncodeToString(value)
 	return base64Str, nil
+}
+
+func XorArrays(a, b []byte) ([]byte, error) {
+	var result []byte
+
+	if len(a) != len(b) {
+		return nil, errors.New("Mismatched array sizes")
+	}
+	if len(a) == 0 {
+		var empty []byte
+		return empty, nil
+	}
+	result = make([]byte, len(a), len(a))
+	for i := 0; i < len(a); i++ {
+		result[i] = a[i] ^ b[i]
+	}
+	return result, nil
 }
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -1,6 +1,7 @@
 package cryptopals
 
 import (
+	"encoding/hex"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,6 +11,36 @@ func TestHexToBase64(t *testing.T) {
 	var base64Str, err = HexToBase64(hexStr)
 	assert.Nil(t, err, "Error Decoding Hex/Encoding Base64")
 	assert.Equal(t, base64Str, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", "Incorrect Base64 Output")
+
+	base64Str, err = HexToBase64("")
+	assert.Nil(t, err, "Error Decoding Hex/Encoding Base64 Empty String")
+	assert.Equal(t, base64Str, "", "Fails with empty string")
+
 	base64Str, err = HexToBase64("BAD_INPUT")
 	assert.NotNil(t, err, "Incorrect error handling")
+}
+
+func TestXorArrays(t *testing.T) {
+	var empty []byte
+	var zero = []byte{0}
+	var one = []byte{1}
+	var result []byte
+	var err error
+
+	result, err = XorArrays(one, one)
+	assert.Nil(t, err)
+	assert.Equal(t, result, zero, "{1} XOR {1} != {0}")
+
+	var x, _ = hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	var y, _ = hex.DecodeString("686974207468652062756c6c277320657965")
+	result, err = XorArrays(x, y)
+	assert.Nil(t, err)
+	assert.Equal(t, hex.EncodeToString(result), "746865206b696420646f6e277420706c6179")
+
+	result, err = XorArrays(empty, empty)
+	assert.Nil(t, err)
+	assert.Equal(t, result, empty, "Fails with empty array")
+
+	result, err = XorArrays(empty, zero)
+	assert.NotNil(t, err, "Failed to reject different sized arrays")
 }
